26afbd8a72e77b1a38d755bf61a711a1c26315d0
richard_ellison <small.car3251@fastmail.com>
2024-07-20 19:56:05 +0100

    x will no longer overwrite the register

--- lua/richard/core/keymaps.lua
+++ lua/richard/core/keymaps.lua
@@ -13,7 +13,7 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
