549e18036db8d8fbcc8521ad644a8efa48b7e0ab
richard_ellison <small.car3251@fastmail.com>
2024-07-29 06:14:56 +0100

    updated telescope
    Its not searching todo lists right now will need to look into why later

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,12 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
+	},
     config = function()
       require("telescope").setup({
         extensions = {
@@ -15,10 +20,23 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      local keymap = vim.keymap
 
+      keymap.set("n", "<leader>tf", builtin.find_files, { desc = "Fuzzy by filename" })
+      keymap.set("n", "<leader>to", builtin.oldfiles, { desc = "Fuzzy search recent files" })
+		keymap.set("n", "<leader>tr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>ts", builtin.live_grep, { desc = "Find string in cwd" })
+		keymap.set({ "n" }, "<leader>tc", builtin.commands, { desc = "Find string under cursor in cwd" })
+		-- keymap.set("n", "<leader>tt", builtin.TodoTelescope, { desc = "Find todos" })
+		keymap.set("n", "<leader>th", builtin.help_tags, { desc = "Help Search" })
+		keymap.set("n", "<leader>tj", builtin.jumplist, { desc = "Search though jumplist history" })
+		keymap.set("n", "<leader>tk", builtin.keymaps, { desc = "Search though keymaps" })
+		keymap.set(
+			"n",
+			"<leader>tb",
+			builtin.current_buffer_fuzzy_find,
+			{ desc = "Search though jumplist history" }
+		)
       require("telescope").load_extension("ui-select")
     end,
   },
