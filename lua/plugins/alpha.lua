afbac2051350cad042a83b3f2df9cf92e0c35a92
richard_ellison <small.car3251@fastmail.com>
2024-08-05 05:34:33 +0100

    added sql ui and dashboard menu changes
    still need to work on getting sql code compleation but managed to
    connect to a postgres database

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -45,10 +45,10 @@ return {
 			dashboard.section.buttons.val = {
 				dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
 				dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-				dashboard.button("SPC tf", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("SPC tf", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 				dashboard.button("SPC ts", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 				-- dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-				dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+				dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 			}
 			return dashboard.opts
 		end
