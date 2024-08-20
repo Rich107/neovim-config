4791f45f3d3c38ad0ec502a859863e5ebc36fd10
richard_ellison <small.car3251@fastmail.com>
2024-08-20 20:58:31 +0100

    updated splits to have a whiteline
    this is only working vertically not horizontally.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,18 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            -- Set up the colorscheme
+            vim.cmd.colorscheme("catppuccin-mocha")
+
+            -- Set the separator color to white
+            vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff", bg = "None", ctermfg = "white", ctermbg = "None" })
+
+            -- Apply the highlight group to window separators
+            vim.o.winhighlight = "VertSplit:WinSeparator"
+        end,
+    },
 }
