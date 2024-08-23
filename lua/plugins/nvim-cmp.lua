92dba1908410085850e791bdf4f1c0e7409769a9
richard_ellison <small.car3251@fastmail.com>
2024-08-23 23:05:09 +0100

    fixed issue with delayed space bar in insert mode

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -41,7 +41,7 @@ return {
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<leader>a"] = cmp.mapping.complete(), -- show completion suggestions
+				-- ["<leader>a"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
