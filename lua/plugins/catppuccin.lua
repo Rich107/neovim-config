70b024af9be867eddec74454be03dc87ef6b7817
richard_ellison <small.car3251@fastmail.com>
2024-08-23 23:05:24 +0100

    formatting

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,18 +1,29 @@
 return {
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            -- Set up the colorscheme
-            vim.cmd.colorscheme("catppuccin-mocha")
-
-            -- Set the separator color to white
-            vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff", bg = "None", ctermfg = "white", ctermbg = "None" })
-
-            -- Apply the highlight group to window separators
-            vim.o.winhighlight = "VertSplit:WinSeparator"
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			-- Set up the colorscheme
+			vim.cmd.colorscheme("catppuccin-mocha")
+			-- Set vertical split color (use WinSeparator in newer Neovim, VertSplit for backward compatibility)
+			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffffff", bg = "None", ctermfg = "white", ctermbg = "None" })
+			-- Set horizontal split (status line) color
+			vim.api.nvim_set_hl(
+				0,
+				"StatusLine",
+				{ fg = "#ffffff", bg = "#000000", ctermfg = "white", ctermbg = "black" }
+			)
+			vim.api.nvim_set_hl(
+				0,
+				"StatusLineNC",
+				{ fg = "#ffffff", bg = "#000000", ctermfg = "white", ctermbg = "black" }
+			)
+			-- Apply the highlight group to window separators
+			-- vim.o.winhighlight = "VertSplit:WinSeparator"
+			vim.o.winhighlight = "WinSeparator:WinSeparator"
+			vim.o.laststatus = 3
+		end,
+	},
 }
