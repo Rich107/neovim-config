995ea7ec990d727778f4198af053fa5947ccbd52
richard_ellison <small.car3251@fastmail.com>
2024-08-27 13:47:49 +0100

    Tree sitter can now detect python functions.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,60 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = { "python" },
+
+				-- Configuring the textobjects module
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							-- Function selection (inner and outer)
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							-- Class selection (inner and outer)
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+	},
 }
