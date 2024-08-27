dc4ba54a375cc8e06d16873be753164b74ab400f
richard_ellison <small.car3251@fastmail.com>
2024-08-27 13:47:18 +0100

    tmux / vim navigation is working

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,11 +9,11 @@ local keymap = vim.keymap -- for conciseness
 
 vim.opt.swapfile = false
 
--- Navigate vim panes better
-keymap.set("n", "<c-k>", ":wincmd k<CR>")
-keymap.set("n", "<c-j>", ":wincmd j<CR>")
-keymap.set("n", "<c-h>", ":wincmd h<CR>")
-keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- -- Navigate vim panes better
+-- keymap.set("n", "<c-k>", ":wincmd k<CR>")
+-- keymap.set("n", "<c-j>", ":wincmd j<CR>")
+-- keymap.set("n", "<c-h>", ":wincmd h<CR>")
+-- keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.wo.number = true
 
 -- scroll gaps at top and bottom of page
