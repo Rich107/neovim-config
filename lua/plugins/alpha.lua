8be3fe8de4824a59065297e76cbb5a479d4c1ca0
richard_ellison <small.car3251@fastmail.com>
2024-09-03 18:05:11 +0100

    removed search files from alpha as it was conflicting

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -45,7 +45,7 @@ return {
 			dashboard.section.buttons.val = {
 				dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
 				dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-				dashboard.button("SPC tf", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+				-- dashboard.button("SPC tf", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 				dashboard.button("SPC ts", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 				-- dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
 				dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
