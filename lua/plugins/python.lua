8ee0bc4841dc16eafe8f972ffccaffa268742160
richard_ellison <small.car3251@fastmail.com>
2025-02-27 13:51:35 +0000

    feat: better python importing
    Fails to install right now hoping that installing neovim in a devpod
    will help.
    
    Doing this as current LSP imports cannot even spot os and import it
    correctly.

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -5,4 +5,21 @@ return {
 			vim.keymap.set("n", "<leader>io", require("lspimport").import, { noremap = true, silent = true })
 		end,
 	},
+	{
+		"kiyoon/python-import.nvim",
+		build = "pipx install . --force",
+		ft = "python",
+		keys = {
+			{
+				"<leader>ip",
+				function()
+					require("python_import.api").add_import_current_word_and_notify()
+				end,
+				mode = { "i", "n" },
+				silent = true,
+				desc = "Add python import",
+				ft = "python",
+			},
+		},
+	},
 }
