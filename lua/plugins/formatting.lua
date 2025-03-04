d4a571e3b807098c384c67e6b51adc88456e7f15
richard_ellison <small.car3251@fastmail.com>
2025-03-04 15:34:45 +0000

    feat: will now format json on save

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,39 +1,44 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-				svelte = { "biome" },
-				css = { "biome" },
-				html = { "biome" },
-				json = { "biome" },
-				yaml = { "biome" },
-				markdown = { "biome" },
-				graphql = { "biome" },
-				liquid = { "biome" },
-				lua = { "stylua" },
-				python = { "ruff" },
-			},
-			format_on_save = {
-				lsp_fallback = false,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "biome" },
+                typescript = { "biome" },
+                javascriptreact = { "biome" },
+                typescriptreact = { "biome" },
+                svelte = { "biome" },
+                css = { "biome" },
+                html = { "biome" },
+                json = { "biome" },
+                yaml = { "biome" },
+                markdown = { "biome" },
+                graphql = { "biome" },
+                liquid = { "biome" },
+                lua = { "stylua" },
+                python = { "ruff" },
+            },
+            format_on_save = {
+                lsp_fallback = false,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.json",
+            callback = function()
+                vim.cmd("%!prettier --stdin-filepath %")
+            end,
+        })
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
