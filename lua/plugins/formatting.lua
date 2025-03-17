c696e77b8030767c1bc9d953ff0aa784450e5ac9
richard_ellison <small.car3251@fastmail.com>
2025-03-17 13:00:15 +0000

    formatting changes and treesitter config change

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,44 +1,60 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "biome" },
-                typescript = { "biome" },
-                javascriptreact = { "biome" },
-                typescriptreact = { "biome" },
-                svelte = { "biome" },
-                css = { "biome" },
-                html = { "biome" },
-                json = { "biome" },
-                yaml = { "biome" },
-                markdown = { "biome" },
-                graphql = { "biome" },
-                liquid = { "biome" },
-                lua = { "stylua" },
-                python = { "ruff" },
-            },
-            format_on_save = {
-                lsp_fallback = false,
-                async = false,
-                timeout_ms = 1000,
-            },
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.json",
-            callback = function()
-                vim.cmd("%!prettier --stdin-filepath %")
-            end,
-        })
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "biome" },
+				typescript = { "biome" },
+				javascriptreact = { "biome" },
+				typescriptreact = { "biome" },
+				svelte = { "biome" },
+				css = { "biome" },
+				html = { "biome" },
+				json = { "biome" },
+				yaml = { "biome" },
+				markdown = { "biome" },
+				graphql = { "biome" },
+				liquid = { "biome" },
+				lua = { "stylua" },
+				python = { "ruff" },
+			},
+			format_on_save = {
+				lsp_fallback = false,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+		-- Not going with this as it moved the curosr on run:
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		-- 	pattern = "*.json",
+		-- 	callback = function()
+		-- 		vim.cmd("%!prettier --stdin-filepath %")
+		-- 	end,
+		-- })
+
+		-- Map <leader>ff to format JSON with Prettier
+		vim.keymap.set("n", "<leader>ff", function()
+			local filetype = vim.bo.filetype
+			if filetype == "json" then
+				vim.cmd("%!prettier --stdin-filepath %")
+			else
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end
+		end, { desc = "Format JSON with Prettier or use Conform for other filetypes" })
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
