0b69b4037aa7fac364913f1f11f75a2d437f02f8
Rich107 <small.car3251@fastmail.com>
2025-04-09 23:54:00 +0100

    making vim test smarter
    ish

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -14,16 +14,30 @@ return {
 		vim.keymap.set("n", "<leader>ra", ":TestSuite<CR>", { desc = "TestSuite - Run all the tests" })
 		vim.keymap.set("n", "<leader>rl", ":TestLast<CR>", { desc = "TestLast - Runs the last test again" })
 		vim.keymap.set("n", "<leader>rv", ":TestVisit<CR>", { desc = "Test Visit - The last test you ran" })
-		vim.cmd("let test#strategy = 'vimux'")
+		if vim.env.HOME == "/Users/richardellison" then
+			vim.cmd("let test#strategy = 'vimux'")
+		else
+			vim.cmd("let test#strategy = 'nvim'")
+		end
 		vim.cmd("let test#python#runner = 'pytest'")
 
 		-- This is working for Legl app (python only)
 
-		if not vim.env.HOME == "/Users/richardellison" and vim.env.PROJECT_ID == "live-personal-pa-leaderboard" then
+		if vim.env.HOME ~= "/Users/richardellison" and vim.env.PROJECT_ID == "live-personal-pa-leaderboard" then
+			-- running on container for pa project
+			vim.cmd("let g:test#python#pytest#executable='pytest --disable-warnings -vv '")
+		elseif vim.env.HOME == "/Users/richardellison" and vim.env.PROJECT_ID == "live-personal-pa-leaderboard" then
+			-- running on host for pa project
 			vim.cmd("let g:test#python#pytest#executable='docker compose exec rest-api pytest --disable-warnings -vv '")
-		elseif not vim.env.HOME == "/Users/richardellison" then
+		elseif vim.env.HOME == "/Users/richardellison" and vim.env.PROJECT_ID ~= "live-personal-pa-leaderboard" then
+			-- running on host for legl project
+			vim.cmd(
+				"let g:test#python#pytest#executable='docker compose exec server pytest --disable-warnings -vv --create-db'"
+			)
+		elseif vim.env.HOME ~= "/Users/richardellison" and vim.env.PROJECT_ID ~= "live-personal-pa-leaderboard" then
+			-- running on container for legl project
 			vim.cmd([[
-                    let g:test#python#pytest#executable = 'docker compose exec server pytest --disable-warnings -vv --create-db'
+                    let g:test#python#pytest#executable = 'pytest --disable-warnings -vv --create-db'
                     ]])
 		else
 			vim.cmd("let g:test#python#pytest#executable='docker compose exec rest-api pytest --disable-warnings -vv '")
