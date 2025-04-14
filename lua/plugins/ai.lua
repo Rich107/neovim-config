0df8b961c6d6b5c298a0674dafb624db418a2e8e
Rich107 <small.car3251@fastmail.com>
2025-04-14 23:13:50 +0100

    Install copilot

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,33 @@
+-- return {
+-- 	"jackMort/ChatGPT.nvim",
+-- 	dependencies = {
+-- 		"MunifTanjim/nui.nvim",
+-- 		"nvim-lua/plenary.nvim",
+-- 		"nvim-telescope/telescope.nvim",
+-- 	},
+-- 	config = function()
+-- 		require("chatgpt").setup()
+-- 	end,
+-- }
+--
+--
+
+-- return {
+-- 	"https://github.com/github/copilot.vim.git",
+-- }
+--
+--
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+}
