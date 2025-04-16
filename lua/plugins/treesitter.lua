794f087657a1197331d6b77e709d8590ba19ad69
Rich107 <small.car3251@fastmail.com>
2025-04-16 07:49:59 +0100

    feat; ensure typescript installed
    Without this the syntax highlighting does not work right away for vue
    files and I would need to jump to a ts file before it would work.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
-				ensure_installed = { "vue", "scss", "css", "html", "javascript", "python" },
+				ensure_installed = { "vue", "scss", "css", "html", "javascript", "python", "typescript" },
 				-- Configuring the textobjects module
 				textobjects = {
 					select = {
