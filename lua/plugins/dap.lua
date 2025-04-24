7fa757d39a78ff5f74e3c7f7483a4ae1683fd2a2
Rich107 <small.car3251@fastmail.com>
2025-04-25 00:10:42 +0100

    feat: added dap

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,86 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-telescope/telescope-dap.nvim",
+        "jbyuki/one-small-step-for-vimkind",
+    },
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        --python setup:
+        dap.adapters.python = function(cb, config)
+            if config.request == "attach" then
+                ---@diagnostic disable-next-line: undefined-field
+                local port = (config.connect or config).port
+                ---@diagnostic disable-next-line: undefined-field
+                local host = (config.connect or config).host or "127.0.0.1"
+                cb({
+                    type = "server",
+                    port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+                    host = host,
+                    options = {
+                        source_filetype = "python",
+                    },
+                })
+            else
+                cb({
+                    type = "executable",
+                    command = "/opt/venv/bin/python",
+                    args = { "-m", "debugpy.adapter" },
+                    options = {
+                        source_filetype = "python",
+                    },
+                })
+            end
+        end
+
+        dap.configurations.python = {
+            {
+                -- The first three options are required by nvim-dap
+                type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+                request = "launch",
+                name = "Launch file",
+
+                -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+                program = "${file}", -- This configuration will launch the current file if used.
+                pythonPath = function()
+                    -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+                    -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+                    -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+                    local cwd = vim.fn.getcwd()
+                    if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+                        return cwd .. "/venv/bin/python"
+                    elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+                        return cwd .. "/.venv/bin/python"
+                    elseif vim.fn.executable("/usr/bin/python") == 1 then
+                        return "/usr/bin/python"
+                    else
+                        return "/opt/venv/bin/python"
+                    end
+                end,
+            },
+        }
+
+        vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+        vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+
+        dapui.setup()
+        require("telescope").load_extension("dap")
+    end,
+}
