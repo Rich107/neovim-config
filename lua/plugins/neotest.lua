832186b16c23e1d64fff0201ed5d022fe3f7a24e
Rich107 <small.car3251@fastmail.com>
2025-05-27 22:36:34 +0100

    added missing dependency

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -12,6 +12,7 @@ else
         dependencies = {
             "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
             "nvim-neotest/neotest-python",
         },
