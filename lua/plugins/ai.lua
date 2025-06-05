92836fec605b9da492a855d90265b38b47dcda7b
Rich107 <small.car3251@fastmail.com>
2025-06-05 09:49:28 +0100

    feat: copilot is now working again

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,45 +1,34 @@
--- return {
--- 	"jackMort/ChatGPT.nvim",
--- 	dependencies = {
--- 		"MunifTanjim/nui.nvim",
--- 		"nvim-lua/plenary.nvim",
--- 		"nvim-telescope/telescope.nvim",
--- 	},
--- 	config = function()
--- 		require("chatgpt").setup()
--- 	end,
--- }
---
---
-
--- return {
--- 	"https://github.com/github/copilot.vim.git",
--- }
---
---
 return {
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{
-				"github/copilot.vim",
-				config = function()
-					vim.g.copilot_no_tab_map = false
-				end,
-			},
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-		},
-		build = "make tiktoken",
-		opts = {
-			mappings = {
-				reset = {
-					normal = "<Leader>rrr",
-					insert = false,
-				},
-				complete = {
-					insert = false,
-				},
-			},
-		},
-	},
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            {
+                "zbirenbaum/copilot.lua",
+                cmd = "Copilot",
+                build = ":Copilot auth",
+                config = function()
+                    require("copilot").setup({
+                        suggestion = { enabled = true },
+                        panel = { enabled = true },
+                    })
+                end,
+            },
+            {
+                "nvim-lua/plenary.nvim",
+                branch = "master",
+            },
+        },
+        build = "make tiktoken",
+        opts = {
+            mappings = {
+                reset = {
+                    normal = "<Leader>rrr",
+                    insert = false,
+                },
+                complete = {
+                    insert = false,
+                },
+            },
+        },
+    },
 }
