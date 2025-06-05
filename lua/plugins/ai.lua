0f491c42d3c301d541a18dffe42d3baf843914df
Rich107 <small.car3251@fastmail.com>
2025-06-05 13:54:32 +0100

    enable suggestions

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,34 +1,39 @@
 return {
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            {
-                "zbirenbaum/copilot.lua",
-                cmd = "Copilot",
-                build = ":Copilot auth",
-                config = function()
-                    require("copilot").setup({
-                        suggestion = { enabled = true },
-                        panel = { enabled = true },
-                    })
-                end,
-            },
-            {
-                "nvim-lua/plenary.nvim",
-                branch = "master",
-            },
-        },
-        build = "make tiktoken",
-        opts = {
-            mappings = {
-                reset = {
-                    normal = "<Leader>rrr",
-                    insert = false,
-                },
-                complete = {
-                    insert = false,
-                },
-            },
-        },
-    },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{
+				"zbirenbaum/copilot.lua",
+				cmd = "Copilot",
+				build = ":Copilot auth",
+				config = function()
+					require("copilot").setup({
+						suggestion = {
+							enabled = true,
+							auto_trigger = true,
+							next = "<M-]>",
+							prev = "<M-[>",
+						},
+						panel = { enabled = true },
+					})
+				end,
+			},
+			{
+				"nvim-lua/plenary.nvim",
+				branch = "master",
+			},
+		},
+		build = "make tiktoken",
+		opts = {
+			mappings = {
+				reset = {
+					normal = "<Leader>rrr",
+					insert = false,
+				},
+				complete = {
+					insert = false,
+				},
+			},
+		},
+	},
 }
