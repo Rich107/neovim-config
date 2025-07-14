4b5aaeab61078140f28ff7843c373ad9fdd96602
Rich107 <small.car3251@fastmail.com>
2025-07-14 20:48:17 +0100

    why??? colour sorted

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,7 +1,7 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	-- I'm replacing this plugin with mini-hipatterns, installed as a LazyExtra
-	enabled = false,
+	enabled = true,
 	config = function()
 		require("colorizer").setup()
 	end,
