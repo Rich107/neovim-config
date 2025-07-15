3ed4b8d78040daa1589625191baf8e0141af05c7
Rich107 <small.car3251@fastmail.com>
2025-07-15 23:10:36 +0100

    fix for all the markdown issues

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,13 +1,13 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	icon_provider = "devicons", -- "mini" or "devicons" or internal,,
+	-- For `nvim-treesitter` users.
+	priority = 49,
 
-    -- For `nvim-treesitter` users.
-    priority = 49,
-
-    -- For blink.cmp's completion
-    -- source
-    -- dependencies = {
-    --     "saghen/blink.cmp"
-    -- },
+	-- For blink.cmp's completion
+	-- source
+	-- dependencies = {
+	--     "saghen/blink.cmp"
+	-- },
 }
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,22 +1,22 @@
 return {
-    -- {
-    --     "epwalsh/obsidian.nvim",
-    --     version = "*",
-    --     ft = "markdown",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",
-    --     },
-    --     opts = {
-    --         workspaces = {
-    --             {
-    --                 name = "Personal",
-    --                 path = "~/Documents/Obsidian Vaults/Personal",
-    --             },
-    --             {
-    --                 name = "work",
-    --                 path = "~/Documents/Obsidian Vaults/Legl",
-    --             },
-    --         },
-    --     },
-    -- },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        ft = "markdown",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "Personal",
+                    path = "~/Documents/Obsidian Vaults/Personal",
+                },
+                {
+                    name = "work",
+                    path = "~/Documents/Obsidian Vaults/Legl",
+                },
+            },
+        },
+    },
 }
