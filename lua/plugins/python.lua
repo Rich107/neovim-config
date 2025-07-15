092401a643c01c5689ea69311d18eb165f081cb3
Rich107 <small.car3251@fastmail.com>
2025-07-15 09:25:07 +0100

    no longer need this importer tool

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"stevanmilic/nvim-lspimport",
+		enabled = false,
 		config = function()
 			vim.keymap.set("n", "<leader>io", require("lspimport").import, { noremap = true, silent = true })
 		end,
