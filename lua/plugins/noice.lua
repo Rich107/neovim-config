6a508ff26ac70f65d3bfea5efbb1894c65719296
Rich107 <small.car3251@fastmail.com>
2025-07-25 10:16:35 +0100

    updated nofity background colour

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,34 +1,38 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
-	config = function()
-		require("noice").setup({
-			routes = {
-				{
-					view = "notify",
-					filter = { event = "msg_showmode" },
-				},
-			},
-			sections = {
-				lualine_x = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { fg = "#ff9e64" },
-					},
-				},
-			},
-		})
-	end,
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- add any options here
+    },
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+    },
+    config = function()
+        require("notify").setup({
+            background_colour = "#000000",
+        })
+
+        require("noice").setup({
+            routes = {
+                {
+                    view = "notify",
+                    filter = { event = "msg_showmode" },
+                },
+            },
+            sections = {
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+            },
+        })
+    end,
 }
