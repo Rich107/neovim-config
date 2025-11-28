5b041ee16fcc969ae954aff49576ad5191220899
Rich107 <small.car3251@fastmail.com>
2025-11-28 23:54:59 +0000

    feat: lazgit full window

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -23,6 +23,13 @@ return {
 		keys = {
 			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
 		},
+		config = function()
+			vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+			vim.g.lazygit_floating_window_scaling_factor = 1.0 -- scaling factor for floating window
+			vim.g.lazygit_floating_window_border_chars = {'─','│','─','│','╭','╮','╯','╰'} -- customize lazygit popup window border characters
+			vim.g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
+			vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
